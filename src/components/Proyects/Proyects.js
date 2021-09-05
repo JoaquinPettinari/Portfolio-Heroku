2f8dd76351e014830626a0125e4e00ca62dde07d
JoaquinPettinari <joaquinpettinari@hotmail.com>
2021-09-05 17:32:32 -0300

    Se agrega la generalizacion de todos las secciones

--- src/components/Proyects/Proyects.js
+++ src/components/Proyects/Proyects.js
@@ -1,56 +1,57 @@
-import { Grid, makeStyles, Typography } from "@material-ui/core";
-import Lottie from "react-lottie";
-import { defaultOptions } from "../../lottieConfig";
-import TitleText from "../ui/TitleText";
+import { Divider, Grid, makeStyles, Typography } from "@material-ui/core";
 import ProyectIcon from '../../assets/lotties/proyect.json'
 import LogoUnahur from '../../assets/isologo.svg'
 import { useTranslate as t } from '../../translation';
+import Layout from "../ui/Layout";
 
 const useStyles = makeStyles(() => ({
-    root:{        
-        background: '#eeeeee',
-        minHeight:'60vh'
-    },    
-    containerDescription:{        
-        display:'grid'        
-    },
     titleJobs:{
         fontWeight:'bold'
     },
+    dividerStyle:{
+        margin: '20px 0'
+    }
   }));
 
 function Proyects(){
 
-    const { root, titleJobs, containerDescription } = useStyles();    
+    const { titleJobs, dividerStyle } = useStyles();    
 
     return (
-        <Grid container justifyContent="center" id="proyectContainer" className={root}>
-            <Grid item xs={8} sm={4} md={3} >                
-                <TitleText label={t("buttonProyects")} />
-            </Grid>
-            <Grid container spacing={5} alignContent="center" justifyContent="center">
-                <Grid item xs={8} sm={6}>
-                    <Lottie options={{animationData: ProyectIcon, ...defaultOptions}} />
-                </Grid>
-                <Grid item xs={8} sm={6} className={containerDescription}>
-                    <Grid container spacing={3} alignContent="center">
-                        <Grid item xs={12}>
-                            <Grid item xs={8} sm={6} md={4}>
-                                <img src={LogoUnahur} alt="Logo Unahur"  />
+        <Layout
+            title="buttonProyects"
+            lottieIcon={ProyectIcon}
+            background="true"
+        >
+            {
+                listOfProyects.map(({ image, alt, proyectDescription,proyectRepository, proyectPage, proyectRepositoryValue, proyectPageValue}, index) => {
+                    return(
+                        <Grid container spacing={3} alignContent="center">
+                            <Grid item xs={12}>
+                                <Grid item xs={8} sm={6} md={4}>
+                                    <img src={image} alt={alt}  />
+                                </Grid>
                             </Grid>
-                        </Grid>
                             <Grid item xs={10}>
                             <Typography variant="h6" className={titleJobs} >
-                                {t("proyectDescription")}
-                                <li><a href="https://github.com/unahur-turnos/" rel="noopener noreferrer" target="_blank">{t("proyectRepository")}</a ></li>
-                                <li><a href="https://turnos.unahur.edu.ar/login" rel="noopener noreferrer" target="_blank">{t("proyectPage")}</a></li>
+                                {t(proyectDescription)}
+                                <li><a href={proyectRepositoryValue} rel="noopener noreferrer" target="_blank">{t(proyectRepository)}</a ></li>
+                                <li><a href={proyectPageValue} rel="noopener noreferrer" target="_blank">{t(proyectPage)}</a></li>
                             </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6} className={dividerStyle} >
+                                {index !== listOfProyects.length - 1 && <Divider color="primary"  /> }
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Grid>
+                    )
+                })
+            }
+        </Layout>
     )
 }
+
+const listOfProyects = [
+    {image: LogoUnahur, alt:"Logo unahur", proyectDescription: 'proyectFirstDescription', proyectRepository: 'proyectFirstRepository', proyectPage:'proyectFirstPage', proyectRepositoryValue: 'https://github.com/unahur-turnos/', proyectPageValue:'https://turnos.unahur.edu.ar/login'}
+]
 
 export default Proyects;
